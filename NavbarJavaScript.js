f4b43fcb8dcf2a941d57309b5d47e5815c7c14fa
Miranda Pratt <miranda-a-100@hotmail.com>
2024-09-06 20:09:44 +0100

    Distinguish active classes between navbar and slideshow

--- NavbarJavaScript.js
+++ NavbarJavaScript.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    $('#nav-item-1').addClass("active");
+    $('#nav-item-1').addClass("activeNavbar");
 
     $("#nav-item-1").click(function() {
-        $(".container ul li").children().removeClass("active");
-        $(this).addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $(this).addClass("activeNavbar");
         $('html, body').animate({
             scrollTop:        $("#welcomeSection").offset().top-160
         });
@@ -11,8 +11,8 @@ $(document).ready(function() {
     });
 
     $("#nav-item-2").click(function() {
-        $(".container ul li").children().removeClass("active");
-        $(this).addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $(this).addClass("activeNavbar");
         $('html, body') .animate({
             scrollTop:        $("#aboutMeSection").offset().top-160
         });
@@ -20,8 +20,8 @@ $(document).ready(function() {
     });
 
     $("#nav-item-3").click(function() {
-        $(".container ul li").children().removeClass("active");
-        $(this).addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $(this).addClass("activeNavbar");
         $(' html,body') .animate({
             scrollTop:        $("#mySkillsSection").offset().top-160
         });
@@ -29,8 +29,8 @@ $(document).ready(function() {
     });
 
     $("#nav-item-4").click(function() {
-        $(".container ul li").children().removeClass("active");
-        $(this).addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $(this).addClass("activeNavbar");
         $('html,body ') .animate({
             scrollTop:        $("#universityProjectsSection").offset().top-160
         });
@@ -38,19 +38,17 @@ $(document).ready(function() {
     });
 
     $("#nav-item-5").click(function() {
-        $(".container ul li").children().removeClass("active");
-        $(this).addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $(this).addClass("activeNavbar");
         $('html,body ') .animate({
             scrollTop:        $("#sideProjectsSection").offset().top-160
         });
-
-        console.log("Here");
         return false;
     });
 
     $("#nav-item-6").click(function() {
-        $(".container ul li").children().removeClass("active");
-        $(this).addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $(this).addClass("activeNavbar");
         $('html,body ') .animate({
             scrollTop:        $("#contactSection").offset().top-160
         });
@@ -58,32 +56,32 @@ $(document).ready(function() {
     });
 
     $('#aboutMeSection').waypoint(function() {
-        $(".container ul li").children().removeClass("active");
-        $("#nav-item-2").addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $("#nav-item-2").addClass("activeNavbar");
     }, { offset: "50%" });
 
     $('#mySkillsSection').waypoint(function() {
-        $(".container ul li").children().removeClass("active");
-        $("#nav-item-3").addClass("active");
-    }, { offset: "25%" });
+        $(".container ul li").children().removeClass("activeNavbar");
+        $("#nav-item-3").addClass("activeNavbar");
+    }, { offset: "50%" });
 
     $('#universityProjectsSection').waypoint(function() {
-        $(".container ul li").children().removeClass("active");
-        $("#nav-item-4").addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $("#nav-item-4").addClass("activeNavbar");
     }, { offset: "50%" });
 
     $('#sideProjectsSection').waypoint(function() {
-        $(".container ul li").children().removeClass("active");
-        $("#nav-item-5").addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $("#nav-item-5").addClass("activeNavbar");
     }, { offset: "50%" });
 
     $('#contactSection').waypoint(function() {
-        $(".container ul li").children().removeClass("active");
-        $("#nav-item-6").addClass("active");
+        $(".container ul li").children().removeClass("activeNavbar");
+        $("#nav-item-6").addClass("activeNavbar");
     }, { offset: "100%" });
 
     $('#welcomeSection').waypoint(function() {
-        $(".container ul li").children().removeClass("active");
-        $("#nav-item-1").addClass("active");
-    }, { offset: -50 });
+        $(".container ul li").children().removeClass("activeNavbar");
+        $("#nav-item-1").addClass("activeNavbar");
+    }, { offset: "50%" });
 });
