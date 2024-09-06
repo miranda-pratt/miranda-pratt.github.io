06f55ef90a91add4d0cf41a9eba4a15fb1c5bd53
Miranda Pratt <miranda-a-100@hotmail.com>
2024-09-06 22:12:16 +0100

    Start of responsive topnav

--- NavbarJavaScript.js
+++ NavbarJavaScript.js
@@ -156,3 +156,13 @@ $(document).ready(function() {
         offset: -100
     });
 });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function responsiveTopnav() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
