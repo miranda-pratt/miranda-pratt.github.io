741714e74c618f69acc385a35364578d73c91984
Miranda Pratt <miranda-a-100@hotmail.com>
2024-09-16 13:06:36 +0100

    Image slideshow for thermometer project

--- javascript/SlideshowJavaScript.js
+++ javascript/SlideshowJavaScript.js
@@ -1,6 +1,6 @@
-let slideIndex = [1,1,1,1,1,1,1,1];
+let slideIndex = [1,1,1,1,1,1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8"];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides9"];
 
 if (document.readyState === "loading") {
     // Loading hasn't finished yet
@@ -19,6 +19,7 @@ function displaySlides() {
     showSlides(1, 5);
     showSlides(1, 6);
     showSlides(1, 7);
+    showSlides(1, 8);
 }
 
 function plusSlides(n, no) {
